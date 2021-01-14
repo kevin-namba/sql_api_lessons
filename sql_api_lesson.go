77ebf87fcd0a878c948bc26a5b8c43824f6abf8d
kevin55156 <nambayouhei@keio.jp>
2021-01-14 17:45:25 +0900

    モデル名の変更

--- sql_api_lesson.go
+++ sql_api_lesson.go
@@ -11,26 +11,22 @@ import (
     _ "github.com/go-sql-driver/mysql"
 )
 
-type ResponseData struct {
-    ID string    `json:"ID"`
-    Token string  `json:"token"`
-    Name   string `json:"name"`
-}
 
-type RequestData struct{
-    Name   string `json:"name"`
-}
 
-type UserGetReponse struct{
-    Name   string `json:"name"`
+type UserCreateRequest struct{
+    Name string `json:"name"`
 }
 
 type UserCreateReponse struct{
     Token string  `json:"token"`
 }
 
-type UserUpdateReponse struct{
-    Name   string `json:"name"`
+type UserGetReponse struct{
+    Name string `json:"name"`
+}
+
+type UserUpdateRequest struct{
+    Name string `json:"name"`
 }
 
 
@@ -50,6 +46,10 @@ func RandString(n int) string {
     }
     return string(b)
 }
+
+
+
+
 
 func userGet(w http.ResponseWriter, r *http.Request) {
   //r.Header.Set("Content-Type","application/json")
@@ -74,7 +74,7 @@ func userGet(w http.ResponseWriter, r *http.Request) {
 
     
     defer db.Close()
-    var person UserGetReponse //構造体Responsedata型の変数personを定義
+    var person UserGetReponse 
     for rows.Next() {
         
     err :=rows.Scan(&person.Name)
@@ -88,64 +88,64 @@ func userGet(w http.ResponseWriter, r *http.Request) {
     res, err := json.Marshal(person)
     
     w.Header().Set("Content-Type", "application/json")
-    fmt.Fprint(w, string(res))
+    w.Write(res)
 }
 
 func userCreate(w http.ResponseWriter, r *http.Request){
 
 
 
-//リクエストボディからデータを取得
-  var req RequestData
-  error := json.NewDecoder(r.Body).Decode(&req)
-	if error != nil {
-	fmt.Println(error)
-			return
-	}
-
-
-  fmt.Printf("%sを登録します。",req.Name)
-
-//w.Header().Set("Content-Type", "application/json")
-
-  // db接続
-  db, err := sql.Open("mysql", "root@/lesson1")
-  log.Println("Connected to mysql.")
-  //接続でエラーが発生した場合の処理
-  if err != nil {
-      log.Fatal(err)
-  }
-
-  var  newid string
-  var newtoken string
-  newid =RandString(8)
-  newtoken =RandString(8)
-
-  fmt.Printf("newid:%s,newtoken:%s", newid,newtoken)
-  //データを挿入する
-
-ins, err := db.Prepare("INSERT INTO users(id,token,name) VALUES(?,?,?)")
-if err != nil {
-    log.Fatal(err)
-   }
-
-ins.Exec(newid, newtoken,req.Name)
-defer db.Close()
-
-var person UserCreateReponse 
-person.Token=newtoken
-res, err := json.Marshal(person)
+    //リクエストボディからデータを取得
+      var req UserCreateRequest
+      error := json.NewDecoder(r.Body).Decode(&req)
+        if error != nil {
+        fmt.Println(error)
+                return
+        }
     
-w.Header().Set("Content-Type", "application/json")
-fmt.Fprint(w, string(res))
-     
+    
+      fmt.Printf("%sを登録します。",req.Name)
+    
+    //w.Header().Set("Content-Type", "application/json")
+    
+      // db接続
+      db, err := sql.Open("mysql", "root@/lesson1")
+      log.Println("Connected to mysql.")
+      //接続でエラーが発生した場合の処理
+      if err != nil {
+          log.Fatal(err)
+      }
+    
+      var  newid string
+      var newtoken string
+      newid =RandString(8)
+      newtoken =RandString(8)
+    
+      fmt.Printf("newid:%s,newtoken:%s", newid,newtoken)
+      //データを挿入する
+    
+    ins, err := db.Prepare("INSERT INTO users(id,token,name) VALUES(?,?,?)")
+    if err != nil {
+        log.Fatal(err)
+       }
+    
+    ins.Exec(newid, newtoken,req.Name)
+    defer db.Close()
+    
+    var person UserCreateReponse 
+    person.Token=newtoken
+    res, err := json.Marshal(person)
+        
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(res)
+         
 }
 
 func userUpdate(w http.ResponseWriter, r *http.Request){
 
   xtoken := r.Header.Get("x-token")
 
-  var req RequestData
+  var req UserUpdateRequest
   error := json.NewDecoder(r.Body).Decode(&req)
   if error != nil {
   fmt.Println(error)
